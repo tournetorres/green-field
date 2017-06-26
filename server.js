6f44d5921252550524853fafa0e5bb7e75af84aa
Operation Spark <operationspark@operationsmbp4.cisco>
2017-06-26 16:15:15 -0500

    connected to database

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const mysql = require('mysql');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT;
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'petdetective',
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.listen(PORT, () => console.log('listening on', PORT));
 
+connection.connect((err) => {
+  if (!err) {
+    console.log('Database is connected ... nn');
+  } else {
+    console.error('Error connecting database ... nn');
+  }
+});
+
+app.listen(PORT, () => console.log('listening on', PORT));

25461f5496f0058822c6b67ec457577f8092709b
tournetorres <tournetorres@gmail.com>
2017-06-26 17:51:01 -0500

    Added new key file

--- client/components/petFoundForm.js
+++ client/components/petFoundForm.js
@@ -1,6 +1,6 @@
 angular.module('pet-detective')
   .controller('petFoundFormController', function () {
-    console.log('I am petFoundFormController');
+
   })
   .directive('petFoundForm', function () {
     return {

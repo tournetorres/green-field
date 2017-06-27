6eb05ffaaac926cebabc5b80f57541892cf5f95d
tournetorres <tournetorres@gmail.com>
2017-06-27 16:33:32 -0500

    (feat) - display petFoundForm on page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,41 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ],
-    "rules": {
-        "no-console": ["warn", { allow: ["warn", "error"] }]
-    }
+  "env": {
+    "node": false,
+    "browser": true,
+    "mocha": true
+  },
+  "extends": [
+    "airbnb-base"
+  ],
+  "globals": {
+    "_": true,
+    "google": true,
+    "pc3Hash": true,
+    "angular": true,
+    "InfoBox": true,
+    "$": true
+  },
+  "settings": {
+    "angular": 1
+  },
+  "rules": {
+    "func-names": 0,
+    "prefer-arrow-callback": 0,
+    "no-param-reassign": 0,
+    "no-console": [2, { "allow": ["warn", "error"] }],
+    "comma-dangle": ["error", {
+      "arrays": "only-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "ignore"
+    }],
+    "no-underscore-dangle": [2, { "allow": [] }],
+    "no-use-before-define": ["error", { "functions": false, "classes": true }],
+    "no-tabs": 0,
+    "wrap-iife": ["error", "inside"],
+    "no-underscore-dangle": 0,
+    "radix": ["error", "as-needed"]
+  }
+
 };

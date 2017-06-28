faf1b3a6887317e52ebe0fb8ea082c4c9c361f16
tournetorres <tournetorres@gmail.com>
2017-06-28 16:03:17 -0500

    (feat) Add angular functionality to dropdown

--- client/components/petLostForm.js
+++ client/components/petLostForm.js
@@ -1,10 +1,18 @@
 angular.module('pet-detective')
-  .controller('petLostFormController', function () {
+  .controller('petLostFormController', function ($http, $window) {
     this.formBody;
     this.address;
     this.type;
     this.lost = 'LOST';
     console.log('inside');
+
+    this.data = {
+      singleSelect: null,
+      multipleSelect: [],
+      option1: 'cat',
+      option2: 'dog',
+    };
+    console.log(this.data.singleSelect);
 
     this.submit = function ($http) {
 
